6644e4accbe6c905e011cb2863b51c1207ce5423
modous <mohamed1010@live.nl>
2023-02-22 14:41:59 +0100

    Boek slaat over

--- public/extra.js
+++ public/extra.js
@@ -1,8 +1,9 @@
 
 
 const allTriggers = document.querySelectorAll(".trigger-member")
-const allCards = document.querySelectorAll(".card")
+const allLeftCards = document.querySelectorAll(".leftcard")
 const allCardText = document.querySelectorAll(".cardtext")
+const allRightCards = document.querySelectorAll(".rightcard")
 
 
 allTriggers.forEach((trigger) => {
@@ -11,18 +12,44 @@ allTriggers.forEach((trigger) => {
     const activeCard = document.getElementById(id)
     const activeCard1 = document.getElementById(id + 1)
 
-    allCards.forEach((card) => {
-        card.classList.add('hide')
+    allLeftCards.forEach((leftCard) => {
+        
+        leftCard.classList.add('hide')
     })
     allCardText.forEach((text) => {
         text.classList.add('hide')
     })
+    allRightCards.forEach((rightCard) => {
+        rightCard.classList.add('animatie')
+
+        setTimeout (() => {
+            rightCard.classList.remove('animatie')
+
+        }, "1000")
+    })
 
     activeCard.classList.remove("hide");
-    activeCard1.classList.remove("hide");
+  
+        activeCard1.classList.remove("hide");
+  
 
   });
   // var long = element.dataset.long;
   // console.log(long)
 });
 
+
+
+//Next pagina animatie
+
+// let next = document.querySelector(".animatie")
+// next.addEventListener("click", nextPage)
+
+// function nextPage(){
+//     next.classList.remove("nextpage")
+//     setTimeout (() =>{
+//         next.classList.add("nextpage")
+//     }, "100")
+
+
+// }
