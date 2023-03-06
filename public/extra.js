ffa86054afc3f53b56ab0336bdfc3b51fd38e288
modous <mohamed1010@live.nl>
2023-03-06 10:45:41 +0100

    comments

--- public/extra.js
+++ public/extra.js
@@ -1,5 +1,5 @@
 
-// Het boek 
+// Het boek verdeelt in verschillende delen
 const allTriggers = document.querySelectorAll(".trigger-member")
 const allLeftCards = document.querySelectorAll(".leftcard")
 const allCardText = document.querySelectorAll(".cardtext")
@@ -28,6 +28,7 @@ allTriggers.forEach((trigger) => {
         }, "1000")
     })
 
+    //de gegevens van de member waar je op klikt wordt de "hide" weggehaald zodat je alleen deze te zien krijgt
     activeCard.classList.remove("hide");
         activeCard1.classList.remove("hide");
   });
